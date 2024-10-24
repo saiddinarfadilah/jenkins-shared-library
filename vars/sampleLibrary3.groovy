cef23933753cdb6b667b944e2c2fa1b4852408eb
Said Dinar Fadilah <saiddinarfadilah@gmail.com>
2024-10-24 21:54:50 +0700

    ci(file): add sampleHelper2 & sampleLibrary3

--- vars/sampleLibrary3.groovy
+++ vars/sampleLibrary3.groovy
@@ -1,16 +1,17 @@
 // vars/sampleLibrary.groovy
+import com.example.SampleHelper2
 
 def buildProject(String buildTool) {
     // Memanggil metode buildProject dari SampleHelper2
-    com.example.SampleHelper2.buildProject(buildTool, this)
+    SampleHelper2.buildProject(buildTool, this)
 }
 
 def runTests(String testTool) {
     // Memanggil metode runTests dari SampleHelper2
-    com.example.SampleHelper2.runTests(testTool, this)
+    SampleHelper2.runTests(testTool, this)
 }
 
 def notify(String status, String channel) {
     // Memanggil metode notify dari SampleHelper2
-    com.example.SampleHelper2.notify(status, channel, this)
+    SampleHelper2.notify(status, channel, this)
 }
